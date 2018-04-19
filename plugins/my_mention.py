7108f8e72f607a48a90d82305f88ac0c9926af5b
ss6987 <>
2018-04-19 09:56:08 +0900

    first commit

---
+++ plugins/my_mention.py
@@ -0,0 +1,54 @@
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+from slackbot.bot import default_reply
+
+return_message = [
+    ["かんかん", "みかん"],
+    ["かーんかーん", "み！か！ん！"],
+    ["ビーチスケッチ", "さくらうち！"],
+    ["好きな食べ物", "サンドイッチ！"],
+    ["ご機嫌いかが果南", "ダイブいい感じ～！"],
+    ["ダイヤッホー", "ダイヤッホーーーーーーー！"],
+    ["全速前進", "ヨーソロー！"],
+    ["からのー", "敬礼！"],
+    ["おはヨハネ", "おは善子ー！"],
+    ["だからヨハネよ", "ぴょん！"],
+    ["おはな", "マルーーー！！"],
+    ["みなさーん", "シャイニー！！"],
+    ["今日も一日", "頑張・・・・ルビィ～～～～！！"],
+    ["にっこにっこにー","にっこにっこにー!"],
+    ["252521","にっこにっこにー"],
+    ["だれかたすけてー","ちょっとまっててー！"],
+    ["にゃんにゃんにゃー", "にゃんにゃんにゃー！"],
+    ["ことりのおやつにしてやるぞー！", "ちゅんちゅん！"],
+    ["かしこいかわいい", "えりーちかー！"],
+    ["せーの", "ふぁいとだよっ！"],
+]
+
+
+@respond_to("")
+def listen_func(message):
+    string = message.body["text"]
+    if string in "-list":
+        return_string = ""
+        for i in return_message:
+            return_string = return_string + i[0] + "," + i[1] + "\n"
+        message.send(return_string)
+    else:
+        for i in return_message:
+            if i[0] in string:
+                message.send(i[1])
+
+
+@listen_to("")
+def listen_func(message):
+    string = message.body["text"]
+    if string in "-list":
+        return_string = ""
+        for i in return_message:
+            return_string = return_string + i[0] + "," + i[1] + "\n"
+        message.send(return_string)
+    else:
+        for i in return_message:
+            if i[0] in string:
+                message.send(i[1])
---
+++ slackbot_settings.py
@@ -0,0 +1,7 @@
+API_TOKEN = "xoxb-348584852467-8MhED9J09BKHv8zWVjM8I0mD"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "サンシャイン"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
